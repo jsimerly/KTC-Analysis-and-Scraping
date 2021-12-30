ad8821bb6cc0f78122adcc1e48c85523d92f0e0e
Jacob <simerly81@gmail.com>
2021-12-30 00:33:15 -0500

    model created, scraping almost ready

---
+++ models.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy import Column, Integer, String, TEXT
+
+engine = create_engine('sqlite:///newdb.db', echo=True)
+session = Session(engine)
+Base = declarative_base()
+
+class playerValues(Base):
+    __tablename__ = 'Player Values'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+    pos = Column(String(25))
+    age = Column(Integer)
+    height = Column(String(6))
+    weight = Column(Integer)
+    drafted = Column(String(15))
+    draftClass = Column(String(20))
+    exp = Column(String(10))
+    valueData = Column(TEXT)
+
+
+    def addPlayer(self, id ,name, pos, age, height, weight, drafted, draftClass, exp, data):
+        player = session.query(self).filter(self.id==id).one()
+       
+        if player:
+            player.name = name
+            player.pos = pos
+            player.age = age
+            player.height = height
+            player.weight = weight
+            player.drafted = drafted
+            player.draftClass = draftClass
+            player.exp = exp
+            player.valueData = data
+        else:
+            player = self(id=id, name=name, pos=pos, age=age, height=height, weight=weight, drafted=drafted, draftClass=draftClass, exp=exp, valueData=data)
+            session.add(player)
+            
+    
+    def commit():
+        session.commit()
+
+#Base.metadata.create_all(engine)
+playerValues.addPlayer(playerValues, 1, 'testnew2', 'pos3', 22, '6"4', 220, '2.02', 2019, 'Rookie', r'2{324}')
+playerValues.addPlayer(playerValues, 5, 'Jacffob', 'pos', 22, '6"4', 220, '2.02', 2019, 'Rookie', r'{}')
+playerValues.commit()
--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
 import requests
 import json
 import re
+import time
 from bs4 import BeautifulSoup
 
+
 class Scraper():
+    #Get Page Data
     def _fetch(self, url):
         resp = requests.get(url)
         return resp
@@ -11,33 +14,98 @@ class Scraper():
     def _getSoup(self, resp):
         soup = BeautifulSoup(resp.text, 'html.parser')
         return soup
-
+    
     def _getContent(self, url):
         resp = self._fetch(url)
-        soup = self._getSoup(resp)
+        content = self._getSoup(resp)
+        return content
+
+    def parsePlayerInfoToJson(self, contents):
+        playerName = contents.find('div', {'class': 'player-details-header-content dashboard-header-content dashboard-header-content-left'}).text.strip()
+        position = contents.find('div', {'class':'player-details-header-subtext dashboard-header-subtext'}).findAll('span')[0].text
+        measureables = contents.findAll('div', {'meas-row'})
+        try:
+            age = measureables[0].find('p', {'class':'row-value'}).text.strip()
+            age = age.replace('y.o.', '')
+            height = measureables[2].find('p', {'class':'row-value'}).text.strip().replace(' ', '')
+            weight = measureables[3].find('p', {'class':'row-value'}).text.strip()
+            drafted = measureables[4].find('p', {'class':'row-value'}).text.strip()
+            draftClass = measureables[5].find('p', {'class':'row-value'}).text.strip()
+            exp = measureables[6].find('p', {'class':'row-value'}).text.strip()
+
+        except:
+            print('None measurables for:')
+            print(playerName)
+            age = None
+            height = None
+            weight = None
+            draftClass = None
+            drafted = None
+            exp = None
         
+        info = {
+            'name': playerName,
+            'pos' : position,
+            'age' : age,
+            'height' : height,
+            'weight' : weight,
+            'drafted' : drafted,
+            'draftClass': draftClass,
+            'exp' : exp,
+        }
+
+        return info
+
+    def parseUrlToId(self, url):
+        return url.rsplit('-',1)[1]
+
+
+    def parseSuperFlexToJson(self, contents):
         pattern = re.compile(r'var playerSuperflex', re.MULTILINE | re.DOTALL)
         
-        content = soup.find('script', text=pattern)
-        return content
-    
-    def parseToJson(self, url):
-        content = str(self._getContent(url))
+        content = contents.find('script', text=pattern)
+        content = str(content)
         bottomHalf = content.split("var playerSuperflex = ",1)[1]
         fullCut = bottomHalf.split(';\n        var playerOneQB = ',1)[0]
         js = json.loads(fullCut)
-        return js
-    
-    def getOverallValue(self, data):
-        return data['overallValue']
+        data = js['overallValue']
+        return data
+   
 
+    #Move to next player
+    def _getNextLink(self, contents):
+        adjBlock = contents.find('div', {'class':'pd-overall-adjacent-block'})
+        nextPlayer = adjBlock.find('div', {'class': 'curPlayer'}).find_next_sibling('div')
+        nextLink = nextPlayer.findAll('a', {'class':'adjacentPlayerLink'}, href=True)
+        for a in nextLink:
+            link = a['href']
+        return link
+
+
+    def scrape(self, url, model):
+        contents = self._getContent(url)
+        #save playerContents
+        jsData = self.parseSuperFlexToJson(contents)
+        measurables = self.parsePlayerInfoToJson(contents)
+        id = self.parseUrlToId(url)
+        nextLink = self._getNextLink(contents)
+        print(nextLink)
     
+        
+        time.sleep(2)
+        if nextLink != None:
+            appenedUrl = 'https://keeptradecut.com' + nextLink
+            self.scrape(appenedUrl, 'fuck')
+        
+        print('The End')
+        
 
 
 if __name__ == '__main__':
     scraper = Scraper()
-    data = scraper.parseToJson('https://keeptradecut.com/dynasty-rankings/players/patrick-mahomes-272')
-    oData = scraper.getOverallValue(data)
-    
-    print(oData)
+    content = scraper._getContent('https://keeptradecut.com/dynasty-rankings/players/2023-mid-1st-1096')
+    #print(scraper.parseUrlToId('https://keeptradecut.com/dynasty-rankings/players/jonathan-taylor-634'))
+    scraper.scrape('https://keeptradecut.com/dynasty-rankings/players/wayne-gallman-172', 'fuck')
+
+    #print(test)
 
